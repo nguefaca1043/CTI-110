bd8c4e0feeb8b28a942bd891de2b8af1df586927
nguefaca1043 <40450217+nguefaca1043@users.noreply.github.com>
2018-06-22 12:49:42 -0400

    First Submission : upload assignement P2T1

---
+++ P2T1_SalesPrediction_NguefackTsobgouArland.py
@@ -0,0 +1,13 @@
+# CTI-110
+# P2T1-Sales Prediction
+# Nguefack
+# 6/21/2018
+# Get the projected total sales.
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of the total sales.
+profit = total_sales * 0.23
+
+#display the profit.
+print ('the profit is $', profit)
+
